df67632967aa478f5ea0bbb50ffd5c6d14083724
ccckmit <ccckmit@gmail.com>
2022-10-12 11:46:49 +0800

    add fp:linearSearch.py

--- 08a-enumerate/04-combination/allCombination.py
+++ 08a-enumerate/04-combination/allCombination.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-def combination(A, k):
-	chooses = []
-	c(A, len(A), k, chooses, k)
-
-def c(A, n, k, chooses, kc):
-	# print('chooses=', chooses, 'k=', k)
-	if len(chooses)==kc:
-		print(chooses)
-		return
-	if n <= 0: return
-	c(A,n-1,k,chooses,kc) # C(n-1,k)
-
-	chooses.append(A[n-1])
-	c(A,n-1,k-1,chooses,kc) # C(n-1,k-1)
-=======
 def combination(A, m): # 從 A 陣列中取出 m 個的所有可能性
 	chooses = []
 	c(A, len(A), m, chooses, m)
@@ -23,11 +7,10 @@ def c(A, n, k, chooses, m): # 從 A[0..n] 中選取 k 個補進 chooses，如果
 		print(chooses)
 		return
 	if n <= 0: return
-	c(A,n-1,k,chooses,m) # C(n-1,k)
+	c(A,n-1,k,chooses,m) # C(n-1,k) // A[n-1] 沒取到
 
 	chooses.append(A[n-1])
-	c(A,n-1,k-1,chooses,m) # C(n-1,k-1)
->>>>>>> c84f3b508c09d28b56ecd8cab3509e77b10099c4
+	c(A,n-1,k-1,chooses,m) # C(n-1,k-1) // A[n-1] 有取到
 	del chooses[-1]
 
 combination([1,2,3,4,5], 3)
